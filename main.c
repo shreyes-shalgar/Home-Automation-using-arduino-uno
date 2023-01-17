15d78ab451dcaa6ed3b2ac00ba293fcf1da096ae
shreyes-shalgar <64411880+shreyes-shalgar@users.noreply.github.com>
2023-01-17 13:05:06 +0530

    Update main.c

--- main.c
+++ main.c
@@ -15,42 +15,47 @@ void setup() {// declare the ledPin as an OUTPUT:
   Serial.begin(9600);
 }
 void loop() {
-  int sensor = analogRead(A2);
-  float voltage = (sensor / 1024.0) * 5.0; 
-  float tempC = (voltage - .5) * 100; 
-  float tempF = (tempC * 1.8) + 32;
-  //Serial.print("\ntemp: ");
-  //Serial.print(tempF);
-  if (tempF < hot/*cold*/) { //cold
-    //digitalWrite(2, HIGH);
-    //digitalWrite(3, LOW); 
-    digitalWrite(4, LOW);
-    //Serial.println(" It's Cold.");
-  }
-  else if (tempF >= hot) 
-  { //hot
-    //digitalWrite(2, LOW);
-    //digitalWrite(3, LOW); 
-    digitalWrite(4, HIGH);
-    //Serial.println(" It's Hot.");
-  }
-  //else 
-    { //fine
-    //digitalWrite(2, LOW);
-    //digitalWrite(3, HIGH);
-    //digitalWrite(4, LOW);
-    //Serial.println(" It's Fine.");
-    //} 
-  delay(0);
-  //Serial.println("Welcome to TechPonder LDR Tutorial"); 
-  sensorValue = analogRead(LDR);
-  //Serial.print("\nLDR value: ");
-  //Serial.print(sensorValue); 
-  if (sensorValue < 80)
-    {//Serial.println(" LED light on."); 
-    digitalWrite(led,HIGH); 
-    delay(1000);}
-  else
-  {
-    digitalWrite(led,LOW); 
-    delay(sensorValue);}
+    int sensor = analogRead(A2);
+    float voltage = (sensor / 1024.0) * 5.0; 
+    float tempC = (voltage - .5) * 100; 
+    float tempF = (tempC * 1.8) + 32;
+    //Serial.print("\ntemp: ");
+    //Serial.print(tempF);
+
+    if (tempF < hot/*cold*/) { //cold
+      //digitalWrite(2, HIGH);
+      //digitalWrite(3, LOW); 
+      digitalWrite(4, LOW);
+      //Serial.println(" It's Cold.");
+    }
+
+    else if (tempF >= hot) 
+    { //hot
+      //digitalWrite(2, LOW);
+      //digitalWrite(3, LOW); 
+      digitalWrite(4, HIGH);
+      //Serial.println(" It's Hot.");
+    }
+
+    //else 
+      { //fine
+      //digitalWrite(2, LOW);
+      //digitalWrite(3, HIGH);
+      //digitalWrite(4, LOW);
+      //Serial.println(" It's Fine.");
+      //} 
+    delay(0);
+
+    sensorValue = analogRead(LDR);
+    //Serial.print("\nLDR value: ");
+    //Serial.print(sensorValue); 
+    if (sensorValue < 80)
+      {//Serial.println(" LED light on."); 
+      digitalWrite(led,HIGH); 
+      delay(1000);}
+    else
+    {
+      digitalWrite(led,LOW); 
+      delay(sensorValue);
+      }
+}
